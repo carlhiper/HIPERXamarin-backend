95377c560a23f4f0d45a318127bfe889e650929d
Carl Windfeldt <carl@hiper.se>
2020-03-19 12:43:52 +0100

    Added comments to server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
+//Add tables
 mobileApp.tables.add('UserModel'); // Create a table for 'UserModel' with default settings
 mobileApp.tables.add('GoalModel'); // Create a table for 'GoalModel' with default settings
 mobileApp.tables.add('TeamModel'); // Create a table for 'TeamModel' with default settings
-
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);

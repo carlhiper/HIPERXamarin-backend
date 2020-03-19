dec20c692fd6518abff98c6e30df0bdaf0253e18
Carl Windfeldt <carl@hiper.se>
2020-03-19 09:17:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('UserModel'); // Create a table for 'UserModel' with default settings
+mobileApp.tables.add('GoalModel'); // Create a table for 'GoalModel' with default settings
+mobileApp.tables.add('TeamModel'); // Create a table for 'TeamModel' with default settings
+
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);

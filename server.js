3d24ba59832370312e2b59b822de7bf5071e2584
Carl Windfeldt <carl@hiper.se>
2020-04-16 13:16:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 mobileApp.tables.add('UserModel'); // Create a table for 'UserModel' with default settings
 mobileApp.tables.add('GoalModel'); // Create a table for 'GoalModel' with default settings
 mobileApp.tables.add('TeamModel'); // Create a table for 'TeamModel' with default settings
-mobileApp.tables.add('PostModel'); // Create a table for 'TeamModel' with default settings
+mobileApp.tables.add('PostModel'); // Create a table for 'PostModel' with default settings
+mobileApp.tables.add('ChallengeModel'); // Create a table for 'ChallengeModel' with default settings
 
 
 app.use(mobileApp);
